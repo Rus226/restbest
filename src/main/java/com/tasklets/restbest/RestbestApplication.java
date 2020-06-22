d23d5bc2189ba8b3e5953b52c87a8e3aafc8ff00
Rus226 <sairutdinov@yandex.ru>
2020-06-22 15:37:53 +0400

    initial commit

---
+++ src/main/java/com/tasklets/restbest/RestbestApplication.java
@@ -0,0 +1,13 @@
+package com.tasklets.restbest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbestApplication.class, args);
+	}
+
+}
