67ad0353e51b08e8b7c2137cee5f1c41590485fd
Rus226 <sairutdinov@yandex.ru>
2020-06-22 16:51:03 +0400

    add MessageController and NotFoundException

---
+++ src/main/java/com/tasklets/restbest/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.tasklets.restbest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
